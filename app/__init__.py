41dc9cbe8b314edc4936d21857216790500c88df
liziyang1996 <834086767@qq.com>
2021-04-02 09:56:39 +0800

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+#从flask包中导入Flask类
+from flask import Flask
+
+
+
+#将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
+app = Flask(__name__)
+
+print('等会谁（哪个包或模块）在使用我：',__name__)
+
+
+#从app包中导入模块routes
+from app import routes
