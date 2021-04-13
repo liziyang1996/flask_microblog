a06b3f8ec1c2bf1c3e6356ebd8327b29d15826d1
liziyang1996 <834086767@qq.com>
2021-04-13 11:18:51 +0800

    chapter5 user_login_register finish

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,16 @@ from config import Config
 
 from flask_sqlalchemy import SQLAlchemy#从包中导入类
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 #将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app = Flask(__name__)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
 app.config.from_object(Config)
+
 
 db = SQLAlchemy(app)#数据库对象
 migrate = Migrate(app, db)#迁移引擎对象
