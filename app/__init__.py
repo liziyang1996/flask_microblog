4cbe259fc3c0d06dfb1793fc1de9fd5303c1e089
liziyang1996 <834086767@qq.com>
2021-04-20 10:32:52 +0800

    chapter10 flask_mail finish

--- app/__init__.py
+++ app/__init__.py
@@ -12,16 +12,15 @@ from flask_login import LoginManager
 
 # 将Flask类的实例 赋值给名为 app 的变量。这个实例成为app包的成员。
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)  # 数据库对象
+migrate = Migrate(app, db)  # 迁移引擎对象
 
 login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
-
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)  # 数据库对象
-migrate = Migrate(app, db)  # 迁移引擎对象
 
 # 从app包中导入模块routes
 from app import routes, models, errors
